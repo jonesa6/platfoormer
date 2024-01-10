cced4f0976f679f83dc9ead986268611e1bf7d4c
jonesa6 <156232904+jonesa6@users.noreply.github.com>
2024-01-10 12:57:15 -0800

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -2,15 +2,14 @@ scene.onOverlapTile(SpriteKind.Player, sprites.builtin.forestTiles0, function (s
     tiles.setCurrentTilemap(tilemap`level2`)
 })
 scene.onOverlapTile(SpriteKind.Player, sprites.swamp.swampTile1, function (sprite, location) {
-    scene.cameraShake(5, 5000)
-    game.setGameOverEffect(true, effects.confetti)
-    game.gameOver(true)
+    myCorg.maxJump = 1
+    tiles.setCurrentTilemap(tilemap`level4`)
 })
 scene.onOverlapTile(SpriteKind.Player, sprites.builtin.forestTiles12, function (sprite, location) {
-    game.setGameOverMessage(false, "You hit the ground")
-    game.gameOver(false)
+    game.reset()
 })
-let myCorg = corgio.create(SpriteKind.Player)
+let myCorg: Corgio = null
+myCorg = corgio.create(SpriteKind.Player)
 myCorg.verticalMovement(true)
 myCorg.horizontalMovement(true)
 myCorg.updateSprite(true)
@@ -32,9 +31,9 @@ let mySprite = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     `, SpriteKind.Player)
-mySprite.follow(myCorg, 200)
+mySprite.follow(myCorg, 100)
 tiles.setCurrentTilemap(tilemap`level1`)
 scene.cameraFollowSprite(mySprite)
-myCorg.maxJump = randint(5, 10)
-myCorg.gravity = 300
+myCorg.maxJump = 6
+myCorg.gravity = 500
 myCorg.setStayInScreen(false)
